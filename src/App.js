21b82baba83d0fbce659f4109b427ee64f4882d3
arnaudMaillet <arnaud.maillet@saint-remi.net>
2021-03-27 16:51:00 +0100

    init project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Container, Grid } from '@material-ui/core';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Grid container>
+        <Grid 
+          item 
+          xs={12} 
+          sm={12} 
+          md={4} 
+          lg={3}
+          style={{backgroundColor: 'blue'}}
+          >
+            ABC
+        </Grid>
+        <Grid 
+          item 
+          xs
+          sm
+          md
+          lg
+          style={{backgroundColor: 'red'}}
+          >
+            XYZ
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
