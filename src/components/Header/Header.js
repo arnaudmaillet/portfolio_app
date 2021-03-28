6ecbdd32e72fa35f98aae2d0c7c19cb3656538a7
arnaudMaillet <arnaud.maillet@saint-remi.net>
2021-03-28 15:31:55 +0200

    Update header timeline content

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Typography } from '@material-ui/core'
+import TimelineItem from '@material-ui/lab/TimelineItem'
+import TimelineContent from '@material-ui/lab/TimelineContent';
 
 import MyData from './../../utils/data.js'
-import Timeline from './../Timeline/Timeline.js'
+import Timeline, { CustomTimelineSeparator } from './../Timeline/Timeline.js'
 
 import './Header.scss'
 import ImgProfil from './../../assets/img/profil.jpg'
+
 
 const Header = () => {
     return (
@@ -15,14 +18,52 @@ const Header = () => {
                 <Typography className='profile_name_title'>{MyData.profile.title}</Typography>
             </div>
             <div className='profile_img'>
-                <img src={ImgProfil} alt='picture'/>
+                <img src={ImgProfil} alt='picture' />
             </div>
-            <div className = 'profile_information'>
-                <Timeline icon={MyData.icon.header_timeline}/>
+            <div className='profile_information'>
+                <Timeline icon={MyData.icon.header_timeline}>
+                    <CustomTimelineItem 
+                        title='Nom'
+                        text={MyData.profile.name}/>
+                    <CustomTimelineItem 
+                        title='Age'
+                        text={MyData.profile.age}/>
+                    <CustomTimelineItem 
+                        title='Email'
+                        text={MyData.profile.email}/>
+                    <CustomTimelineItem 
+                        title='Adresse'
+                        text={MyData.profile.adress}/>
+                    
+                </Timeline>
                 <button>Mon CV</button>
             </div>
         </div>
     )
 }
+
+// Custom header's timeline section
+const CustomTimelineItem = ({ title, text, link }) => (
+    <TimelineItem>
+        <CustomTimelineSeparator/>
+        <TimelineContent>
+
+            {/* Conditionnal if link display link else display text*/}
+            {
+                link ? (
+                    <Typography>
+                        <span>{title} :</span>
+                        <a href={link} target='_blank'>{text}</a>
+                    </Typography>
+                ) : (
+                    <Typography>
+                        <span>{title} :</span> {text}
+                    </Typography>
+                )
+                    
+            }
+        </TimelineContent>
+    </TimelineItem>
+)
 
 export default Header
