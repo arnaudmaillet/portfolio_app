408fcfe81ed47706f5d8c8bc10d5bbbb094abce8
arnaudMaillet <arnaud.maillet@saint-remi.net>
2021-04-04 01:07:56 +0200

    Fix: map to data

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -13,7 +13,7 @@ const Map = () => {
                     <Iframe
                         width='100%'
                         height='100%'
-                        url='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2094.7675155657794!2d2.296521506320683!3d49.89343022524075!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e78438b0abd7c3%3A0xff54500fc939881f!2zTHljw6llIFByaXbDqSBTYWludCBSw6ltaQ!5e0!3m2!1sfr!2sfr!4v1617489467835!5m2!1sfr!2sfr" width="400" height="400" style="border:0;" allowfullscreen="" loading="lazy"></iframe>'/>
+                        url={MyData.profile.map}/>
                 </Grid>
             </Grid>
         </div>
--- src/utils/data.js
+++ src/utils/data.js
@@ -35,7 +35,9 @@ export default {
         email: 'arnaud@arnaud.com',
         adress: '12 rue de la rue',
         job: 'etudiant',
-        phone: '0101010101'
+        phone: '0101010101',
+
+        map: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2094.7675155657794!2d2.296521506320683!3d49.89343022524075!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e78438b0abd7c3%3A0xff54500fc939881f!2zTHljw6llIFByaXbDqSBTYWludCBSw6ltaQ!5e0!3m2!1sfr!2sfr!4v1617489467835!5m2!1sfr!2sfr" width="400" height="400" style="border:0;" allowfullscreen="" loading="lazy"></iframe>',
     },
 
     icons: {
