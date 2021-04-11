d08ff016c7a731e8ff35aea05712a65409dd0a0e
arnaudMaillet <arnaud.maillet@saint-remi.net>
2021-04-12 00:57:49 +0200

    Update : Darkmode enabled to formations page

--- src/pages/Formations/Formations.js
+++ src/pages/Formations/Formations.js
@@ -1,10 +1,19 @@
 import React, { useState } from 'react'
-import { Grid, Tabs, Tab, Grow, Card, CardMedia, CardContent, Typography } from '@material-ui/core'
+import { Grid, Tabs, Tab, Grow, Card, CardMedia, CardContent, Typography, makeStyles, Paper } from '@material-ui/core'
 import './Formations.scss'
 import Title from '../../components/Title/Title'
 import MyData from "../../utils/Data.js"
 import LinearWithValueLabel from './../../components/LinearProgress/LinearWithValueLabel.js'
 import CustomButton from "./../../components/Button/Button.js";
+
+const style = makeStyles(theme => ({
+    text: {
+        color: theme.palette.info.main,
+    },
+    borderColor: {
+        border: '1px solid ' + theme.palette.primary.main,
+    }
+}))
 
 const Formations = () => {
 
@@ -12,16 +21,17 @@ const Formations = () => {
 
     const [option, setoption] = useState(0)
 
+    const classes = style()
 
     return (
-        <>
+        <Paper elevation={MyData.settings.cardElevation}>
             {/* School training */}
             <Grid container className='school-training'>
                 <Title classSection='school-training'>{MyData.learning.schoolTraining.title}</Title>
                 <Grid item xs={12}>
                     <Typography className='school-training_item_title'>{MyData.learning.schoolTraining.btsSio.title}</Typography>
                     <div>
-                        <Typography className='school-training_item_text'>{MyData.learning.schoolTraining.btsSio.text}</Typography>
+                        <Typography className={['school-training_item_text', classes.text].join(' ')}>{MyData.learning.schoolTraining.btsSio.text}</Typography>
                     </div>
                     <Grid container>
                         <Grid item xs={12} md={3} style={{ marginTop: '22px' }}>
@@ -31,6 +41,7 @@ const Formations = () => {
                                         className='school-training_item_btn_content'
                                         onClick={() => setoption(0)}>
                                         <CustomButton
+                                            color={option === 0 ? 'primary' : ''}
                                             text={MyData.learning.schoolTraining.btsSio.options[0].title}
                                             icon={option === 0 ? MyData.icons.btn_sio : null}
                                             width='160px'
@@ -42,6 +53,7 @@ const Formations = () => {
                                         className='school-training_item_btn_content'
                                         onClick={() => setoption(1)}>
                                         <CustomButton
+                                            color={option === 1 ? 'primary' : ''}
                                             text={MyData.learning.schoolTraining.btsSio.options[1].title}
                                             icon={option === 1 ? MyData.icons.btn_sio : null}
                                             width='160px'
@@ -68,9 +80,9 @@ const Formations = () => {
                         </Grid>
                         <Grid item xs={12} md={9}>
                             {option === 1 ? (
-                                <Typography className='school-training_item_option'>{MyData.learning.schoolTraining.btsSio.options[0].text}</Typography>
+                                <Typography className={['school-training_item_option', classes.borderColor, classes.text].join(' ')}>{MyData.learning.schoolTraining.btsSio.options[1].text}</Typography>
                             ) : (
-                                <Typography className='school-training_item_option'>{MyData.learning.schoolTraining.btsSio.options[1].text}</Typography>
+                                <Typography className={['school-training_item_option', classes.borderColor, classes.text].join(' ')}>{MyData.learning.schoolTraining.btsSio.options[0].text}</Typography>
                             )}
                         </Grid>
                     </Grid>
@@ -116,7 +128,7 @@ const Formations = () => {
                                 {tabValue === learning.tag || tabValue === 'all' ? (
                                     <Grid item xs={12}>
                                         <Grow in timeout={1000}>
-                                            <Card className='self-learning_custom-card'>
+                                            <Card elevation={MyData.settings.cardElevation} className={'self-learning_custom-card'}>
                                                 <CardMedia className='self-learning_custom-card_img' image={learning.img} title={learning.title} />
                                                 <CardContent className='self-learning_custom-card_content'>
                                                     <Grid container>
@@ -127,9 +139,10 @@ const Formations = () => {
                                                             </div>
                                                         </Grid>
                                                     </Grid>
-                                                    <Typography variant='body2' className='self-learning_custom-card_content_text'>{learning.description}</Typography>
+                                                    <Typography variant='body2' className={['self-learning_custom-card_content_text', classes.text].join(' ')}>{learning.description}</Typography>
                                                     <div className='self-learning_custom-card_content_button'>
                                                         <CustomButton
+                                                            color='primary'
                                                             textDisplay='flex'
                                                             justifyText='center'
                                                             href={learning.link}
@@ -145,7 +158,7 @@ const Formations = () => {
                     </Grid>
                 </Grid>
             </Grid>
-        </>
+        </Paper>
     )
 }
 
